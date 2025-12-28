9df5f73d8cc2b7e9aa820be8974a094c775f0785
AKIL <akilsudhagar7@gmail.com>
2025-12-28 21:15:55 +0530

    Configure for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import SettingsPage from './pages/SettingsPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
